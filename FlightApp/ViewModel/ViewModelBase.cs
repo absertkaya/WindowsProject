06bcbe28c251f5de86c31df49ae9ce2af6bb600d
Abdullah Sertkaya <abdullah.sertkaya@student.hogent.be>
2019-10-16 10:22:26 +0200

    ViewModelBase Added

---
+++ FlightApp/ViewModel/ViewModelBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace FlightApp.ViewModel
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public DataTemplate Template { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ViewModelBase()
+        {
+            Template = GetTemplate();
+        }
+
+        private DataTemplate GetTemplate()
+        {
+            string s = GetType().Name;
+            return (DataTemplate)App.Current.Resources[s];
+        }
+
+        protected void RaisePropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
