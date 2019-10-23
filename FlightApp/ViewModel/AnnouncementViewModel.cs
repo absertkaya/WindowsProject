54e250820b0408f71d4295ea8ba39d40c880c5db
Abdullah Sertkaya <abdullah.sertkaya@student.hogent.be>
2019-10-23 09:48:50 +0200

    api call webservice announcement

--- FlightApp/ViewModel/AnnouncementViewModel.cs
+++ FlightApp/ViewModel/AnnouncementViewModel.cs
@@ -1,8 +1,11 @@
 ﻿using FlightApp.Data;
 using FlightApp.Model;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,7 +18,18 @@ namespace FlightApp.ViewModel
         public AnnouncementViewModel()
         {
             //call api
-            Announcements = new DummyData().Announcements;
+            Announcements = new ObservableCollection<Announcement>();
+        }
+
+        public async void CallService()
+        {
+            HttpClient client = new HttpClient();
+            var json = await client.GetStringAsync(new Uri("https://localhost:44300/api/announcements/get_all"));
+            var lst = JsonConvert.DeserializeObject<IList<Announcement>>(json);
+            foreach (var announcement in lst)
+            {
+                Announcements.Add(announcement);
+            }
         }
     }
 }
