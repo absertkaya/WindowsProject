5650cad775bf3b7f66216a898ee3f8610feb1f0f
unknown <bono-opalfvens@hotmail.com>
2019-11-20 17:18:44 +0100

    Edit ShopLayout + Cart layout

--- FlightApp/ViewModel/ShopViewModel.cs
+++ FlightApp/ViewModel/ShopViewModel.cs
@@ -2,17 +2,30 @@
 using FlightApp.Model;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Windows.UI.Popups;
 
 namespace FlightApp.ViewModel
 {
     public class ShopViewModel : ViewModelBase
     {
-        private IList<Product> _products;
-        public IList<Product> Products
+        private IList<Product> _foodProducts;
+        private IList<Product> _drinkProducts;
+        private IList<Product> _snackProducts;
+        public IList<Product> FoodProducts
         {
-            get { return _products; }
-            set { _products = value; RaisePropertyChanged(); }
+            get { return _foodProducts; }
+            set { _foodProducts = value; RaisePropertyChanged(); }
+        }
+        public IList<Product> DrinkProducts
+        {
+            get { return _drinkProducts; }
+            set { _drinkProducts = value; RaisePropertyChanged(); }
+        }
+        public IList<Product> SnackProducts
+        {
+            get { return _snackProducts; }
+            set { _snackProducts = value; RaisePropertyChanged(); }
         }
 
         public ShopViewModel()
@@ -24,7 +37,10 @@ namespace FlightApp.ViewModel
         {
             try
             {
-                Products = await ShopRepository.GetAllProductsAsync();
+                IList<Product> products = await ShopRepository.GetAllProductsAsync();
+                FoodProducts = products.Where(p => p.Type.Equals(ProductType.FOOD)).ToList();
+                DrinkProducts = products.Where(p => p.Type.Equals(ProductType.DRINKS)).ToList();
+                SnackProducts = products.Where(p => p.Type.Equals(ProductType.SNACKS)).ToList();
             }
             catch (Exception e)
             {
